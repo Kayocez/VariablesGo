5d2ecbd5c0aa7d2f7b63c95ec79e39adf04e3ae1
kayocez <kayocezar02@gmail.com>
2026-05-02 19:24:33 +0100

    Training Golang

---
+++ variaveis.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	num1 := 1
+
+	var numero1 int = 2
+
+	fmt.Println(num1)
+
+	fmt.Println(numero1)
+
+	//---------------------------------------------------
+
+	var nome string = "kayo"
+	euMermo := "Kayo Cezar"
+
+	fmt.Println(nome)
+	fmt.Println(euMermo)
+
+	//---------------------------------------------------
+
+	var booleano bool = false
+	maiorDeIdade := true
+
+	fmt.Println(booleano)
+	fmt.Println(maiorDeIdade)
+
+	//---------------------------------------------------
+
+	var altura float32 = 1.70
+	Altura1 := 1.70
+
+	fmt.Println(altura)
+	fmt.Println(Altura1)
+}
